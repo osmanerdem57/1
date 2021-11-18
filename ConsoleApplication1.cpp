40dba91db291d8690c653fb5b9a6364b81cb7ee1
osmanerdem57 <55737410+osmanerdem57@users.noreply.github.com>
2021-11-18 17:40:23 +0300

    Add files via upload
    Gladyatör kılıç oyunu

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+//2^n<s    (s-2^n)*2+1=hayatta kalan kişi sırası
+int main()
+{
+	setlocale(LC_ALL, "turkish");
+
+	int s, x = 1, kalan;
+	tekrar:
+	cout << "Gladyatör sayısını giri: ";
+	cin >> s;
+
+	for (;;)
+	{
+		if (x < s)
+		{
+			x = x * 2;
+		}
+		else if (x == s)
+		{
+			cout << "Hayatta kalan kşinin sırası: 1";
+			break;
+		}
+		else if (x == s)
+		{
+			cout << "Hayatta kalan kşinin sırası: 1" << endl;
+			break;
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (x > s)
+	{
+		x = x / 2;
+		kalan = ((s - x) * 2) + 1;
+		cout << kalan;
+
+	}
+	cout<<endl <<"x: "<< x <<"s: "<< s << endl;
+	goto tekrar;
+	
+
+}
