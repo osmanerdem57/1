cb054434c1f2e06ebcebddfdc861e829998f7c20
osmanerdem57 <55737410+osmanerdem57@users.noreply.github.com>
2021-11-18 17:36:49 +0300

    Add files via upload

---
+++ Asal-sayilarin-toplami.cpp
@@ -0,0 +1,20 @@
+﻿// Asal-sayilarin-toplami.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	unsigned int i, k, toplam=2;
+	for (i = 3;i < 20000;i++)
+	{
+		for (k = 2;k < i;k++)
+		{
+			if (i % k == 0)
+				break;
+			if (k == i - 1)
+				toplam += i;
+		}
+	}
+	cout << toplam;
+}
