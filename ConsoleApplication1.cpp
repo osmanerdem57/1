4ab6cbeb62f16cc7313d451fbe43b4a621b4c975
osmanerdem57 <55737410+osmanerdem57@users.noreply.github.com>
2021-11-18 17:41:26 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,45 +1,76 @@
-﻿#include <iostream>
+#include <iostream>
+#include <time.h>
+#include <locale.h>
+#include <random>
 using namespace std;
-//2^n<s    (s-2^n)*2+1=hayatta kalan kişi sırası
 int main()
 {
 	setlocale(LC_ALL, "turkish");
 
-	int s, x = 1, kalan;
-	tekrar:
-	cout << "Gladyatör sayısını giri: ";
-	cin >> s;
-
-	for (;;)
+	int	r,i,rnd, t, k, i1, cevap, oyna, ysayac = 0, dsayac = 0, adet;
+	srand(time(NULL));
+	cout << "Kaç tane çözmek istersiniz: ";
+	cin >> adet;
+	time_t baslangic_zamani = time(NULL);
+	for ( r=1 ; r <= adet; r++)
 	{
-		if (x < s)
+		rnd = rand() % 10 + 1;
+		k = rand() % 4 + 2;
+		i1 = rand() % 2 + 1;
+		t = rand() % 10 + 1;
+													/*
+													cout << k << "x" << endl;
+													cout << i1 << " ( 1 --> +"<< t <<" ) ( 2 --> -"<<t<<" )"<< endl;
+													*/
+		cout << "1. adım " << rnd << endl;
+		for ( i=2 ; i < 4; i++)
 		{
-			x = x * 2;
+			if (i1 == 1)
+			{
+				rnd = rnd * k + t;
+				cout << i << ". adım " << rnd << endl;
+			}
+			else 
+			{
+				rnd = rnd * k - t;
+				cout << i << ". adım " << rnd << endl;
+			}
 		}
-		else if (x == s)
+		if (i1 == 1)
 		{
-			cout << "Hayatta kalan kşinin sırası: 1";
-			break;
-		}
-		else if (x == s)
-		{
-			cout << "Hayatta kalan kşinin sırası: 1" << endl;
-			break;
+
+			rnd = rnd * k + t;
+
 		}
 		else
 		{
-			break;
+			rnd = rnd * k - t;
+		}
+		cout << "4. adım ?";
+		cevapla:
+		cout << endl << "soru işareti yerine ne gelmelidir: ";
+		cin >> cevap;
+		if (cevap == rnd)
+		{
+			cout << "Tebrikler bildiniz." << endl;
+			dsayac++;
+			cout << "*******************************************************************" << endl;
+		}
+		else
+		{
+			cout << "Yanlış cevap.";
+			ysayac++;
+			goto cevapla;
 		}
 	}
-	if (x > s)
-	{
-		x = x / 2;
-		kalan = ((s - x) * 2) + 1;
-		cout << kalan;
+	time_t bitis_zamani = time(NULL);
+	double saniye = (double)(bitis_zamani - baslangic_zamani);
+	cout << "10 soruda doğru yanlış sayınız " << endl << "Doğru cevap sayısı: " << dsayac << endl << "Yanlış cevap sayısı: " << ysayac;
+	cout << endl << "Cevap Süreniz : " << saniye << " Saniyedir."<<endl<<endl;
+	cout << "Oynadığınız için teşekkürler umarım eğlenmişsinizdir." <<endl<< endl;
+	system("pause");
 
-	}
-	cout<<endl <<"x: "<< x <<"s: "<< s << endl;
-	goto tekrar;
-	
+	//oynarken geçen süeyi yazdırmada Ahmet Bohur dan yardım aldım
 
+	//Dijital Oyun Tasarımı 1B Osman Erdem 1902020031
 }
