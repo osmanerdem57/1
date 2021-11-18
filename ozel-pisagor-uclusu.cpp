c26153659bb078f625390f2bad6bc742d48e0492
osmanerdem57 <55737410+osmanerdem57@users.noreply.github.com>
2021-11-18 17:41:58 +0300

    Add files via upload

---
+++ ozel-pisagor-uclusu.cpp
@@ -0,0 +1,34 @@
+﻿// ozel-pisagor-uclusu.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int a, b, c,islem=0;
+	for (c=5; ;c++)
+	{
+		for (b = 4;b < c;b++)
+		{
+			for (a = 3;a < b;a++)
+			{
+				if (pow(a, 2) + pow(b, 2) == pow(c, 2)&& a+b+c==1000)
+				{
+					cout << a << " " << b << " " << c;
+						islem = a * b * c;
+						cout << endl << islem;
+						break;
+				}
+				if (islem > 1)
+					break;
+			}
+			if (islem > 1)
+				break;
+		}
+		if (islem > 1)
+			break;
+	}
+	
+}
+
