6ee57b4b613b721ac161fe3222b4a6eab3d7dc52
osmanerdem57 <55737410+osmanerdem57@users.noreply.github.com>
2021-11-18 17:36:00 +0300

    Add files via upload

---
+++ 3ve5inkatlari.cpp
@@ -0,0 +1,37 @@
+﻿// 3ve5inkatlari.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int sinir, i;
+	tekrar:
+	cout << "Sınırı belirleyiniz: ";
+	cin >> sinir;
+	if (sinir < 3)
+	{
+		cout << "girdiğiniz sınırda 3 ve 5 in katı yoktur"<<endl;
+		goto tekrar;
+	}
+	for (i = 1;i < sinir + 1;i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			cout << " " << i;
+		}
+	}
+
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
