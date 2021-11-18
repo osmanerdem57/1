363239ac27194319b9e87a66672ede96569e745b
osmanerdem57 <55737410+osmanerdem57@users.noreply.github.com>
2021-11-18 17:37:57 +0300

    Add files via upload

---
+++ en-buyuk-asal-carpan.cpp
@@ -0,0 +1,35 @@
+﻿// en-buyuk-asal-carpan.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "turkish");
+	int sayi, i;
+	cout << "bir değer giriniz: ";
+	cin >> sayi;
+	for (i = 2;i <= sayi;i++)
+	{
+		if (sayi % i == 0)
+			cout << endl;
+		while (sayi % i == 0)
+		{
+			cout << i << " ";
+			sayi = sayi / i;
+		}
+		
+		
+	}
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
