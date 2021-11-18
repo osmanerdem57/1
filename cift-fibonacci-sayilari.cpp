74f697c066eee083eb7f819da7ee2cbfadb65955
osmanerdem57 <55737410+osmanerdem57@users.noreply.github.com>
2021-11-18 17:37:28 +0300

    Add files via upload

---
+++ cift-fibonacci-sayilari.cpp
@@ -0,0 +1,35 @@
+﻿// cift-fibonacci-sayilari.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "turkish");
+	unsigned int a = 0, b = 1, sayi, c = 1;
+	cout << "Kaçıncı değere kadar gitsin: ";
+	cin >> sayi;
+	for (int i = 0;i < sayi-2;i++)
+	{
+		if (c % 2 == 0)
+		{
+			cout << endl << c;
+		}
+		a = b;
+		b = c;
+		c = a + b;
+
+	}
+
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
